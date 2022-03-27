5820c556736175b74b877b9620e3955fa451f6a8
thuythuytiennguyen <thuytienthuytien134@gmail.com>
2022-03-26 22:11:12 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (callback, i ) => {
+  return tutorials.map( i => i.split(' ').map(
+    function(word){
+      return(word.charAt(0).toUpperCase() + word.slice(1));
+    }
+  ).join(' '))
 }
